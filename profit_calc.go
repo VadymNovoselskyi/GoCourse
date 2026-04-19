5c64760b05f14a21ba175b35df66f50e3ec32959
Vadym Novoselskyi <novoselskyi.vadim@gmail.com>
2026-04-19 16:33:18 +0200

    complete an exercise...

--- profit_calc.go
+++ profit_calc.go
@@ -1,26 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
 func calculateProfits() {
-	revenue := getInput("Enter revenue: ")
-	expenses := getInput("Enter expenses: ")
-	taxRate := getInput("Enter taxRate: ")
+	revenue, err := getPositiveInput("Enter revenue: ")
+	if err != nil {
+		fmt.Println("Invalid input for revenue:", err)
+		return
+	}
 
-	earningsBeforeTax, earningsAfterTax, taxRatio := _calcProfitValues(revenue, expenses, taxRate)
+	expenses, err := getPositiveInput("Enter expenses: ")
+	if err != nil {
+		fmt.Println("Invalid input for expenses:", err)
+		return
+	}
 
-	fmt.Printf("Earning before tax: %.2f\n", earningsBeforeTax)
-	fmt.Printf("Earning after tax: %.2f\n", earningsAfterTax)
-	fmt.Printf("tax ratio: %.2f\n", taxRatio)
+	taxRate, err := getPositiveInput("Enter taxRate: ")
+	if err != nil {
+		fmt.Println("Invalid input for tax rate:", err)
+		return
+	}
+
+	earningsBeforeTax, earningsAfterTax, taxRatio := calcProfitValues(revenue, expenses, taxRate)
+	earningsBeforeTaxStr := fmt.Sprintf("Earning before tax: %.2f\n", earningsBeforeTax)
+	earningsAfterTaxStr := fmt.Sprintf("Earning after tax: %.2f\n", earningsAfterTax)
+	taxRatioStr := fmt.Sprintf("tax ratio: %.2f\n", taxRatio)
+
+	fmt.Print(earningsBeforeTaxStr)
+	fmt.Print(earningsAfterTaxStr)
+	fmt.Print(taxRatioStr)
+
+	os.WriteFile("profit-calculations.txt", []byte(earningsBeforeTaxStr+earningsAfterTaxStr+taxRatioStr), 0644)
 }
 
-func getInput(inputText string) (variable float64) {
+func getPositiveInput(inputText string) (float64, error) {
+	var inputValue float64
 	fmt.Print(inputText)
-	fmt.Scan(&variable)
-	return
+	fmt.Scan(&inputValue)
+
+	if inputValue <= 0 {
+		return 0, errors.New("Input must be a positive number")
+	}
+
+	return inputValue, nil
 }
 
-func _calcProfitValues(revenue, expenses, taxRate float64) (float64, float64, float64) {
+func calcProfitValues(revenue, expenses, taxRate float64) (float64, float64, float64) {
 	earningsBeforeTax := revenue - expenses
 	earningsAfterTax := earningsBeforeTax * (1 - taxRate/100.0)
 	taxRatio := earningsBeforeTax / earningsAfterTax
